04a481cae9044374110587efcee273b54fb08bf8
Ardalan Amini <ardalan1074@gmail.com>
2018-08-11 19:21:02 +0430

    Update method.ts

--- src/string/camelCase/method.ts
+++ src/string/camelCase/method.ts
@@ -1,6 +1,6 @@
 const method = (str: string) => {
   str = (
-    str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g) || [""]
+    str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+|[A-Z]|[0-9]+/g) || [""]
   ).map((x) => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase()).join("");
 
   return str.slice(0, 1).toLowerCase() + str.slice(1);
