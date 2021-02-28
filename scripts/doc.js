2f6ba983dba4ac5d82ff6d4a9c863859e460e0ef
Ardalan Amini <ardalanamini22@gmail.com>
2021-02-28 20:29:01 +0330

    chore: Update dependencies

--- scripts/doc.js
+++ scripts/doc.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const path = require("path");
-// const rimraf = require("rimraf");
 const { execSync } = require("child_process");
 const fs = require("fs");
 
@@ -9,13 +8,6 @@ const startTime = new Date().getTime();
 
 const DOCS_DIR = path.resolve(__dirname, "..", "docs");
 const DOCS_PATH = path.resolve(DOCS_DIR, "README.md");
-
-// rimraf.sync(DOCS_DIR);
-
-// fs.mkdirSync(DOCS_DIR);
-
-// fs.writeFileSync(path.resolve(DOCS_DIR, "CNAME"), "prototyped.js.org", "utf8");
-// fs.writeFileSync(path.resolve(DOCS_DIR, "_config.yml"), "theme: jekyll-theme-cayman", "utf8");
 
 execSync(path.resolve(__dirname, "..", "node_modules", ".bin", "tsc"));
 
